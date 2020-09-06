f6f955bb3c2c0783f34476fbb3bac05b2e807b60
MazieKor <maciej.coders@gmail.com>
2020-09-06 19:24:04 +0200

    Task 1

--- src/main/java/pl/coderslab/mysql/javamysql/Main01.java
+++ src/main/java/pl/coderslab/mysql/javamysql/Main01.java
@@ -1,5 +1,11 @@
 package pl.coderslab.mysql.javamysql;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main01 {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DBUtil.connect();
+    }
 
 }
